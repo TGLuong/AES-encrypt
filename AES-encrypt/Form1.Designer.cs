65aaa2071fe21c42665cfab7c03f07c8c2c87b75
gialuong <gialuong9820@gmail.com>
2021-11-11 20:59:55 +0700

    update form

--- AES-encrypt/Form1.Designer.cs
+++ AES-encrypt/Form1.Designer.cs
@@ -30,12 +30,10 @@
         {
             this.panelMenu = new System.Windows.Forms.Panel();
             this.panelLogo = new System.Windows.Forms.Panel();
-            this.acs_enc = new System.Windows.Forms.Button();
             this.panelTitle = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.aes_enc = new System.Windows.Forms.Button();
+            this.panelDesktop = new System.Windows.Forms.Panel();
             this.panelMenu.SuspendLayout();
             this.panelTitle.SuspendLayout();
             this.SuspendLayout();
@@ -43,10 +41,7 @@
             // panelMenu
             // 
             this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
-            this.panelMenu.Controls.Add(this.button3);
-            this.panelMenu.Controls.Add(this.button2);
-            this.panelMenu.Controls.Add(this.button1);
-            this.panelMenu.Controls.Add(this.acs_enc);
+            this.panelMenu.Controls.Add(this.aes_enc);
             this.panelMenu.Controls.Add(this.panelLogo);
             this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelMenu.Location = new System.Drawing.Point(0, 0);
@@ -62,21 +57,6 @@
             this.panelLogo.Name = "panelLogo";
             this.panelLogo.Size = new System.Drawing.Size(220, 75);
             this.panelLogo.TabIndex = 0;
-            // 
-            // acs_enc
-            // 
-            this.acs_enc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(76)))));
-            this.acs_enc.Dock = System.Windows.Forms.DockStyle.Top;
-            this.acs_enc.FlatAppearance.BorderSize = 0;
-            this.acs_enc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.acs_enc.ForeColor = System.Drawing.Color.Gainsboro;
-            this.acs_enc.Location = new System.Drawing.Point(0, 75);
-            this.acs_enc.Name = "acs_enc";
-            this.acs_enc.Size = new System.Drawing.Size(220, 60);
-            this.acs_enc.TabIndex = 1;
-            this.acs_enc.Text = "ACS Encrypt";
-            this.acs_enc.UseVisualStyleBackColor = false;
-            this.acs_enc.Visible = false;
             // 
             // panelTitle
             // 
@@ -101,56 +81,34 @@
             this.label1.Text = "Advanced Encryption Standard";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // button1
+            // aes_enc
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(76)))));
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button1.Location = new System.Drawing.Point(0, 135);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(220, 60);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "ACS Encrypt";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Visible = false;
+            this.aes_enc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.aes_enc.Dock = System.Windows.Forms.DockStyle.Top;
+            this.aes_enc.FlatAppearance.BorderSize = 0;
+            this.aes_enc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aes_enc.ForeColor = System.Drawing.Color.Gainsboro;
+            this.aes_enc.Location = new System.Drawing.Point(0, 75);
+            this.aes_enc.Name = "aes_enc";
+            this.aes_enc.Size = new System.Drawing.Size(220, 60);
+            this.aes_enc.TabIndex = 1;
+            this.aes_enc.Text = "AES Encrypt";
+            this.aes_enc.UseVisualStyleBackColor = false;
             // 
-            // button2
+            // panelDesktop
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(76)))));
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button2.Location = new System.Drawing.Point(0, 195);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(220, 60);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "ACS Encrypt";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Visible = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(76)))));
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.ForeColor = System.Drawing.Color.Gainsboro;
-            this.button3.Location = new System.Drawing.Point(0, 255);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(220, 60);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "ACS Encrypt";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Visible = false;
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(220, 75);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(701, 444);
+            this.panelDesktop.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(921, 519);
+            this.Controls.Add(this.panelDesktop);
             this.Controls.Add(this.panelTitle);
             this.Controls.Add(this.panelMenu);
             this.Name = "Form1";
@@ -165,13 +123,11 @@
         #endregion
 
         private System.Windows.Forms.Panel panelMenu;
-        private System.Windows.Forms.Button acs_enc;
         private System.Windows.Forms.Panel panelLogo;
         private System.Windows.Forms.Panel panelTitle;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button aes_enc;
+        private System.Windows.Forms.Panel panelDesktop;
     }
 }
 
